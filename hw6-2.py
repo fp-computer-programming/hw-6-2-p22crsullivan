be8020b29d37fb1826e8f634bfc6937c69caf4a2
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-11-09 10:14:10 -0500

    Submit HW 6-2

---
+++ hw6-2.py
@@ -0,0 +1,30 @@
+# Author: CRS 11/09/21
+# Question 1
+lst = [0, 1, 2, 3, 4, 5, 6, 7]
+
+# Question 2
+print(len(lst))
+
+# Question 3
+print(lst[2])
+
+# Question 4
+print(5 not in lst)
+
+# Question 5
+print(10 not in lst)
+
+# Question 6
+lst2 = ["Chris", "Mateo", "Spencer"]
+
+# Question 7
+print(lst2[0])
+
+# Question 8
+print(lst2 * 3)
+
+# Question 9
+print(lst + lst2)
+
+# Question 10
+print(len(lst + lst2))
